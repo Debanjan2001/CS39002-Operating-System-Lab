85b229f2a9c0e30817d2e736250aeb836a90872f
Debanjan Saha <debanjansaha2001@gmail.com>
2022-01-18 17:46:48 +0530

    Learning scripting is fun!

---
+++ Assignment-1/file1.sh
@@ -0,0 +1,1 @@
+factor $1 | cut -c 5-
---
+++ Assignment-1/file2.sh
@@ -0,0 +1,22 @@
+mkdir -p 1.b.files.out
+for FILE in 1.b.files/*; do 
+    sort -n $FILE > "1.b.files.out/$(basename $FILE)";
+done
+
+#----------------------
+# declare -A freq
+# for FILE in 1.b.files.out/*; do
+#     # cat $FILE >> 1.b.out.txt
+#     for NUM in $(cat $FILE); do
+#         if [ -v freq[$NUM] ];then
+#             freq[$NUM] = freq[$NUM] + 1
+#         else
+#             freq[$NUM] = 0;
+#         fi
+#     done 
+# done
+#----------------------
+
+cat 1.b.files.out/*.txt >> temp;
+sort -n temp | uniq -c | awk ' { t = $1; $1 = $2; $2 = t; print; } ' > 1.b.out.txt
+rm temp
