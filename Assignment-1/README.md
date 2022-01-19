f01cfe0653120f61ec361f6c03568582a5f3e5e0
Debanjan Saha <debanjansaha2001@gmail.com>
2022-01-19 12:58:39 +0530

    Added upto 1.d with extraction cmd in README

---
+++ Assignment-1/README.md
@@ -0,0 +1,16 @@
+## Extraction Commands
+
+- 1.b
+``` 
+unzip 1.b.files.zip 
+```
+
+- 1.c
+```
+ mkdir data1c && tar -xvzf data1c.tar.gz -C data1c/
+```
+
+- 1.d
+```
+unzip data1d.zip -d data1d/
+```
--- Assignment-1/file3.sh
+++ Assignment-1/file3.sh
@@ -1,23 +1,12 @@
-# ITS WORKING BUT TAKING INFINITE TIME, NEED TO CHANGE
-
 mkdir -p 1.c.out
-CNT=0
 
+EXTENSIONS=$(find data1c -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u)
 
-for FILE in $(find data1c -type f); do
-    filename=$(basename -- "$FILE")
-    extension=${FILE##*.}
-    if [ $extension == $FILE ]; then
-        mkdir -p "1.c.out/Nil"
-        mv $FILE "1.c.out/Nil/$filename"
-    else
-        mkdir -p "1.c.out/$extension"
-        mv $FILE "1.c.out/$extension/$filename"
-    fi
-
-    ((CNT=CNT+1))
-    if ((CNT%1000==0)); then 
-        echo $CNT
-    fi
+for EXT in $EXTENSIONS; do
+    mkdir -p "1.c.out/$EXT"
+    find data1c/ -type f -name "*.$EXT" -exec mv -t "1.c.out/$EXT/" {} +
+    echo "$EXT done"
 done;
 
+mkdir -p 1.c.out/Nil
+find data1c/ -type f -exec mv -t "1.c.out/Nil/" {} +
