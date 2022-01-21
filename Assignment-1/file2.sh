aef6b01f564853d7ac2ca544b2ff98be2bbad2b8
Debanjan Saha <debanjansaha2001@gmail.com>
2022-01-21 16:23:04 +0530

    Reduce wc

--- Assignment-1/file2.sh
+++ Assignment-1/file2.sh
@@ -2,21 +2,4 @@ mkdir -p 1.b.files.out
 for FILE in 1.b.files/*; do 
     sort -n $FILE > "1.b.files.out/$(basename $FILE)";
 done
-
-#----------------------
-# declare -A freq
-# for FILE in 1.b.files.out/*; do
-#     # cat $FILE >> 1.b.out.txt
-#     for NUM in $(cat $FILE); do
-#         if [ -v freq[$NUM] ];then
-#             freq[$NUM] = freq[$NUM] + 1
-#         else
-#             freq[$NUM] = 0;
-#         fi
-#     done 
-# done
-#----------------------
-
-cat 1.b.files.out/*.txt >> temp;
-sort -n temp | uniq -c | awk '{print $2,$1}' > 1.b.out.txt
-rm temp
+sort -n 1.b.files.out/*.txt | uniq -c | awk '{print $2,$1}' > 1.b.out.txt
