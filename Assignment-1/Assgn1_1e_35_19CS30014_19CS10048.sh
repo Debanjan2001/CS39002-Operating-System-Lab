96dbdea43ec6c04cf03747f4a9ae2146d9b8eada
Pritkumar Godhani <pritgodhani132@gmail.com>
2022-01-23 21:59:38 +0530

    Renamed Files

--- Assignment-1/Assgn1_1e_35_19CS30014_19CS10048.sh
+++ Assignment-1/Assgn1_1e_35_19CS30014_19CS10048.sh
@@ -11,19 +11,17 @@ while getopts ":v" OPT;do
    esac
 done
 
-log "Setting env variable REQ_HEADERS..."
+log "Setting env_variable REQ_HEADERS..."
 export REQ_HEADERS="Accept,Connection"
 
 log "GET-ting example.com to example.html..."
 curl -s --get https://example.com --output example.html
-echo "example.html file downloaded."
 
 log "GET-ting IP and Response Headers..."
 curl -s -i --get http://ip.jsontest.com/ 
 
 log "GET-ting Headers..."
 FETCHED_HEADERS=`curl -s --get http://headers.jsontest.com/`
-echo $FETCHED_HEADERS
 
 log "Checking for REQ_HEADERS..."
 for HEADER in ${REQ_HEADERS//,/ };do
@@ -35,14 +33,18 @@ for HEADER in ${REQ_HEADERS//,/ };do
     fi
 done
 
-log "Checking validity of JSON files..."
-echo -n > ./1.e.files/valid.txt 
-echo -n > ./1.e.files/invalid.txt
+log "Checking validity of JSON..."
+echo -n > valid.txt 
+echo -n > invalid.txt
 for FILE in 1.e.files/*.json;do
     log ">>Validating $(basename $FILE)..."
     if [ $(curl -s -d "json=`cat $FILE`" -X POST http://validate.jsontest.com/ | jq ".[\"validate\"]") = "true" ];then
-        echo $(basename $FILE) >> ./1.e.files/valid.txt
+        echo $(basename $FILE) >> valid.txt
     else 
-        echo $(basename $FILE) >> ./1.e.files/invalid.txt
+        echo $(basename $FILE) >> invalid.txt
     fi
+
+sort valid.txt -o valid.txt
+sort invalid.txt -o invalid.txt
+
 done
