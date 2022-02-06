fe9d85dcd5063fb10e946c30827b77496c717fcc
Pritkumar Godhani <pritgodhani132@gmail.com>
2022-02-06 23:39:11 +0530

    "DONE:: MultiWatch"

--- Assignment-2/shell.cpp
+++ Assignment-2/shell.cpp
@@ -14,6 +14,7 @@ using namespace std;
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <poll.h>
 #include <unistd.h>
 #include <cstring>
 #include <cstdlib>
@@ -25,6 +26,7 @@ struct termios original;
 
 #define BACKSPACE 127
 #define MAXHISTSIZE 1000
+#define MAX_BUFFER_SIZE 1024
 
 vector<pid_t> waitingFor;
 static sigjmp_buf backtoprompt;
@@ -159,9 +161,10 @@ int tokenizeCommand(string& command, Command* cmd){
 	}
 
 	if(foundQuote != false){
-		cout<<"ParseError(): Invalid Command\n";
+		cerr<<"ERROR:: Invalid Command\n";
 		cmd->tokens.clear();
-		exit(EXIT_FAILURE);
+        return -1;
+		// exit(EXIT_FAILURE);
 	}
 
     vector<string> args_cmd;
@@ -295,14 +298,16 @@ void sigtstpHandler(int signo) {
     pause();
 }
 
-int executeSimpleCommand(Command* cmd, int inFD, int outFD) {
-    cout<<"Simple CMD : "<<cmd->enteredCmd<<endl;
+int executeSimpleCommand(Command* cmd, int inFD, int outFD, int mw_mode = 0) {
+    // cout<<"Simple CMD : "<<cmd->enteredCmd<<endl;
     pid_t pid, pgid = 0;
     pid = fork();
     if(pid == 0) {
-        pid = getpid();
-        if(pgid == 0) pgid = pid;
-        setpgid(pid, pgid);
+        if(!mw_mode) {
+            pid = getpid();
+            if(pgid == 0) pgid = pid;
+            setpgid(pid, pgid);
+        }
         if(!cmd->isBackground)
             tcsetpgrp(shell_terminal, pgid);
         signal (SIGINT, SIG_DFL);
@@ -325,19 +330,21 @@ int executeSimpleCommand(Command* cmd, int inFD, int outFD) {
         exit(status);
     }
     else {
-        if(!pgid) pgid = pid;
-        setpgid(pid, pgid);
+        if(!mw_mode) {
+            if(!pgid) pgid = pid;
+            setpgid(pid, pgid);
+        }
     }
 
     if(!cmd->isBackground) {
         tcsetpgrp(shell_terminal, pgid);
         int status;
         pid_t x = waitpid(pid, &status, WUNTRACED);
-        cout<<"Wait Status : "<<status<<endl;
-        cout<<"Waiting stopped.."<<endl;
+        // cout<<"Wait Status : "<<status<<endl;
+        // cout<<"Waiting stopped.."<<endl;
         if(!WIFEXITED(status) && WIFSTOPPED(status)) {
-            cout<<"Stopped by : "<<WSTOPSIG(status)<<endl;
-            cout<<"Resuming child["<<pid<<"] now..."<<x<<endl;
+            // cout<<"Stopped by : "<<WSTOPSIG(status)<<endl;
+            // cout<<"Resuming child["<<pid<<"] now..."<<x<<endl;
             if(kill(-pid, SIGCONT) < 0) {
                 cerr<<"ERROR:: transmission of SIGCONT failed."<<endl;
             }
@@ -354,7 +361,7 @@ int executeSimpleCommand(Command* cmd, int inFD, int outFD) {
 }
 
 
-int executeCommand(Command* cmd) {
+int executeCommand(Command* cmd, int mw_mode = 0) {
     int numArgs = cmd->tokens.size()-1;
 
     if(cmd->infilename.length() > 0 && ((cmd->inFile = open(cmd->infilename.c_str(), O_RDONLY)) < 0)) {
@@ -370,7 +377,7 @@ int executeCommand(Command* cmd) {
     if(cmd->isPipe) {
         // Implement piped executions
         int status = 1;
-        cout<<"pipe commands"<<endl;
+        // cout<<"pipe commands"<<endl;
         int pipeFD[2];
         int inFD = cmd->pipeCmds[0]->inFile;
         int i = 0;
@@ -380,7 +387,7 @@ int executeCommand(Command* cmd) {
                 cerr<<"ERROR:: pipe() failed."<<endl;
                 pipeErr = 1;
             } else {
-                status = executeSimpleCommand(cmd->pipeCmds[i], inFD, pipeFD[1]);
+                status = executeSimpleCommand(cmd->pipeCmds[i], inFD, pipeFD[1], mw_mode);
                 close(pipeFD[1]);
                 inFD = pipeFD[0];
             }
@@ -388,12 +395,12 @@ int executeCommand(Command* cmd) {
         // put not error later
         if(!pipeErr) {
             //cout<<"And finish."<<endl;
-            status = executeSimpleCommand(cmd->pipeCmds[i], inFD, cmd->pipeCmds[i]->outFile);
+            status = executeSimpleCommand(cmd->pipeCmds[i], inFD, cmd->pipeCmds[i]->outFile, mw_mode);
         }
         return status;
     } 
     else {
-        return executeSimpleCommand(cmd, cmd->inFile, cmd->outFile);
+        return executeSimpleCommand(cmd, cmd->inFile, cmd->outFile, mw_mode);
     }
 
 }
@@ -452,6 +459,64 @@ int searchHistory(deque<string>& history, string& searchstr) {
     }
 }
 
+int parseCommand(string entered_cmd, vector<Command*>& watchedCmds) {
+    string cmd = trim(entered_cmd);
+    int closeSqB = cmd.find(']');
+    if(closeSqB == string::npos) {
+        cerr<<"ERROR:: incorrect multiwatch syntax, missing \']\'"<<endl;
+        return -1;
+    }
+    int nextcloseSqB = cmd.find(']', closeSqB+1);
+    if(nextcloseSqB != string::npos) {
+        cerr<< "ERROR:: incorrect multiwatch syntax, extra \']\'"<<endl;
+        return -1;
+    }
+    int openSqB = cmd.find('[');
+    if(openSqB == string::npos) {
+        cerr<<"ERROR:: incorrect multiwatch syntax, missing \'[\'"<<endl;
+        return -1;
+    }
+    int nextopenSqB = cmd.find('[', openSqB+1);
+    if(nextopenSqB != string::npos) {
+        cerr<< "ERROR:: incorrect multiwatch syntax, extra \'[\'"<<endl;
+        return -1;
+    }
+    if(openSqB >= closeSqB) {
+        cerr<< "ERROR:: incorrect multiwatch syntax, \']\' precedes \'[\'"<<endl;
+        return -1;
+    }
+
+    string cmdliststr = cmd.substr(openSqB+1,closeSqB-openSqB-1);
+    // cout<<cmdliststr<<endl;
+
+    vector<string> cmdlist;
+    splitAmpersand(cmdliststr, cmdlist, ",");
+
+    for(auto s:cmdlist) {
+        if((*(s.begin()) != '\"')&&(*(s.end()-1) != '\"')) {
+            cerr<< "ERROR:: incorrect multiwatch syntax"<<endl;
+            return -1;
+        }
+        else {
+            // cout<<">"<<s<<"<"<<endl;
+            string cmdstr ;
+            if(s == *(cmdlist.end()-1))
+                cmdstr = s.substr(1,s.length()-2);
+            else 
+                cmdstr = s.substr(1,s.length()-3);
+            // cout<<"cmd:"<<cmdstr<<endl;
+            Command* nextcmd = new Command(cmdstr);
+            if(parseCommand(nextcmd->enteredCmd, nextcmd) < 0) {
+                cerr<<"WARNING:: skipping cmd["<<cmdstr<<"]"<<endl;
+                continue;
+            } 
+            nextcmd->isBackground = true;
+            watchedCmds.push_back(nextcmd);
+        }
+    }
+    return 0;
+}
+
 int handleMultiwatch(string entered_cmd) {
     // start
     // Parse commands into a vector<Command*>
@@ -464,9 +529,125 @@ int handleMultiwatch(string entered_cmd) {
     // keep printing until all child do not stop
     // do the pgid voodoo here to stop on ctrl c
     // end
-
     // doubt : how will these process take inputs ? or do we assume them to work without inputs
-    return 0;
+    pid_t surrogate, surrogate_grp = 0;
+    surrogate = fork();
+    if(surrogate == 0) {
+        surrogate = getpid();
+        if(!surrogate_grp) surrogate_grp = surrogate;
+        setpgid(surrogate, surrogate_grp);
+        tcsetpgrp(shell_terminal, surrogate_grp);
+        signal(SIGINT, SIG_DFL);
+        
+        vector<Command*> watchedCmds;
+        // cout<<entered_cmd<<endl;
+        
+        if(parseCommand(entered_cmd, watchedCmds)<0) {
+            exit(-1);
+        }
+
+        int numWatched = watchedCmds.size();
+        // cout<<numWatched<<endl;
+
+        vector<int> writefds;
+        vector<int> readfds;
+        
+        int pipes[numWatched][2];
+        for(int i = 0; i < numWatched; i++) {
+            if(pipe(pipes[i]) < 0) {
+                cerr<<"ERROR:: pipe() failed."<<endl;
+                exit(-1);
+            }
+            int fd = pipes[i][1];
+            writefds.push_back(fd);
+
+            int rfd = pipes[i][0];
+            readfds.push_back(rfd);
+        }
+        vector<pid_t> processes;
+        for(int i = 0; i < numWatched; i++) {
+            pid_t pid;
+            pid = fork();
+            if(pid == 0) {
+                dup2(writefds[i], STDOUT_FILENO);
+                close(writefds[i]);
+
+                executeCommand(watchedCmds[i], 1);
+                exit(1);
+            } else {
+                processes.push_back(pid);
+            }
+        } 
+
+        int nfds, openfds;
+        struct pollfd* pollfdset;
+        openfds = nfds = numWatched;
+
+        pollfdset = (struct pollfd*)calloc(nfds, sizeof(struct pollfd));
+        if(pollfdset == NULL) {
+            cerr<<"ERROR:: calloc() failed."<<endl;
+        }
+
+        for(int i = 0; i < nfds; i++) {
+            pollfdset[i].fd = pipes[i][0];
+            pollfdset[i].events = POLLIN;
+        }
+
+        while(openfds > 0) {
+            int ready = poll(pollfdset, nfds, -1);
+            if(ready == -1) {
+                cerr<<"ERROR:: bad poll()."<<endl;
+                exit(-1);
+            }
+            
+            for(int j = 0; j < nfds; j++) {
+                char buffer[MAX_BUFFER_SIZE];
+                if (pollfdset[j].revents != 0) {
+                    // printf("  fd=%d; events: %s%s%s\n", pollfdset[j].fd, (pollfdset[j].revents & POLLIN)  ? "POLLIN "  : "",(pollfdset[j].revents & POLLHUP) ? "POLLHUP " : "",(pollfdset[j].revents & POLLERR) ? "POLLERR " : "");
+                    if (pollfdset[j].revents & POLLIN) {
+                        memset(buffer, 0, MAX_BUFFER_SIZE);
+                        ssize_t s = read(pollfdset[j].fd, buffer, sizeof(buffer));
+                        if (s == -1) {
+                            cerr<<"ERROR:: read() failed."<<endl;
+                        }
+                        cout<<endl;
+                        cout<<"\""<<watchedCmds[j]->enteredCmd<<"\", current_time:"<<(unsigned long)time(NULL)<<endl;
+                        cout<<"<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-<-"<<endl;
+                        cout<<buffer;
+                        cout<<"->->->->->->->->->->->->->->->->->->->"<<endl<<endl;
+                        // printf("    read %zd bytes: %.*s\n",s, (int) s, buffer);
+                    } else {                /* POLLERR | POLLHUP */
+                        // printf("    closing fd %d\n", pollfdset[j].fd);
+                        if (close(pollfdset[j].fd) == -1) {
+                            cerr<<"ERROR:: close() failed."<<endl;
+                        }
+                        openfds--;
+                    }
+                }
+            }
+        }
+        
+        return 0;
+    }
+    else {
+        if(!surrogate_grp) surrogate_grp = surrogate; 
+        setpgid(surrogate, surrogate_grp);
+
+        tcsetpgrp(shell_terminal, surrogate_grp);
+        int status;
+        pid_t x = waitpid(surrogate, &status, WUNTRACED);
+        // cout<<"Wait Status : "<<status<<endl;
+        // cout<<"Waiting stopped.."<<endl;
+        
+        tcsetpgrp(shell_terminal, shell_pgid);
+        struct termios job_tmodes;
+        tcgetattr(shell_terminal, &job_tmodes);
+        tcsetattr(shell_terminal, TCSADRAIN, &shell_tmodes);
+        if(WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        }
+        return 0;
+    }
 }
 
 void testParser() {
@@ -536,24 +717,23 @@ int main () {
 			break;
 		}
         if (enteredcmd.compare("history") == 0) {
-            cout << "Showing history..." << endl;
+            // cout << "Showing history..." << endl;
             for(string x: history) {
                 cout<<x<<endl;
             }
             continue;
         }
-        if(enteredcmd.compare("multiwatch") == 0) {
-            cout<<"Starting multiwatch..."<<endl;
-            if(handleMultiwatch(enteredcmd)) {
-
+        stringstream ss(enteredcmd);
+        string cmdName; ss>>cmdName;
+        if(cmdName.compare("multiwatch") == 0) {
+            // cout<<"Starting multiwatch..."<<endl;
+            if(handleMultiwatch(enteredcmd) < 0) {
+                cerr<<"Multiwatch failed"<<endl;
             }
+            signal (SIGINT, SIG_IGN);
+            continue;
         }
-
-        // check for  ctrl r
-
         // implement utilities like cd, exit, help, etc
-
-        // implement multiwatch
 
         Command* cmd = new Command(enteredcmd);
         if(parseCommand(cmd->enteredCmd, cmd) < 0) {
