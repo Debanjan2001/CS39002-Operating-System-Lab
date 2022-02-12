7d5b903a32d09b17a40c67faff041f789c1d0523
Pritkumar Godhani <pritgodhani132@gmail.com>
2022-02-12 23:52:52 +0530

    TASK1 :: Seems okay. Part B pending.

---
+++ Assignment-3/task2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+using namespace std;
+int main() {
+    
+}
