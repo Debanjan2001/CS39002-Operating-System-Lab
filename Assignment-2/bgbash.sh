62c70ec8f05fed1e66d8bf190cc4adc977ef758a
Debanjan Saha <debanjansaha2001@gmail.com>
2022-02-12 14:26:00 +0530

    Implement :: Our Own Shell (#2)
    * skeleton added
    
    * Basic Pipe, BG working
    
    * Basic Pipe, BG working
    
    * CTRL_C works, avoid CTRL_Z
    
    * CTRL Z also working
    
    * minor changes
    
    * "DONE:: MultiWatch"
    
    * "FULLY DONE :: jinga-la-la"
    
    * "FINAL:: few tweaks"
    
    * Some minor changes
    
    * "Now finally done :)"
    
    Co-authored-by: Pritkumar Godhani <pritgodhani132@gmail.com>

---
+++ Assignment-2/bgbash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NUM=20
+for((i=2;i<=$NUM;i++));do
+    echo $i
+    sleep 1
+done
---
+++ Assignment-2/hello1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int main() 
+{
+    cout<<"hello, please enter number : "<<endl;
+    int d;
+    cin>>d;
+    cout<<"you entered : "<<d<<endl;
+    cout<<"b..bye"<<endl;
+}
